c46286001ebb5fffc0a44ff2d1ff9fbac81db9ea
ashibinsamw <ashibinsam@gmail.com>
2023-12-07 13:19:13 +0530

    anagram first commit with one test case wrong

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length) {
+    return false;
+  }
+  for(let char1 in str1) {
 
+    let isPresent = false;
+
+    for(let char2 in str2) {
+
+      if(char1 == char2) {
+        isPresent = true;
+      }
+
+    }
+    if(isPresent ==false) {
+      return false;
+    }
+    else {
+      isPresent = false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;

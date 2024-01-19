9d16b39b331d21b1a38b1948be3cdf0373791106
ashibinsam <ashibinsam@gmail.com>
2024-01-19 20:35:02 +0530

    in my knowledge, I haveve completed the code database and admin routes of the mongodb assignment of the week 3 without any testing

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,27 +1,50 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://ashibinsam:3tzsAvD2oUUFiZpl@cluster0.1jibl45.mongodb.net/course-app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+        courseid: Number,
+        title: String,
+        discription: String,
+        price: Number,
+        imageLink: String
+    
 });
+
+const IndexSchema = new mongoose.Schema({
+    type: String,  // used to refer what index does the object maintains
+    value: Number
+})
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
+const Index = mongoose.model('Index', IndexSchema);
 
+//creating a new instance of the Admin object to store the data in the structure specified by the schema.
+// data = {
+//     username: 'Shibin Sam',
+//     password: 'Hi there I am sam'
+// }
+// let admin = new Admin(data);
+
+// admin.save();
+// //TODO: remove the dummy save and write proper code here.
 module.exports = {
     Admin,
     User,
-    Course
+    Course,
+    Index
 }
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,36 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course, Index} = require("../db");
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+    let username = req.username;
+    let password = req.password;
+    const admin = new Admin({username, password});
+    res.json('Admin created successfully');
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    let courseid = await Index.findOne({type: 'course'}).exec();
+    let title = req.body.title;
+    let discription = req.body.discription;
+    let price = req.body.price;
+    let imageLink = req.body.imageLink;
+    let courseDetails = { courseid, title, discription, price, imageLink}
+
+    if(!(await Course.findOne({title}).exec())) {
+        new Course(courseDetails);
+    }
+    courseid.value++;
+    await courseid.save();
+    
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async (req, res) => {
+    let courses = Course.find({});
+    res.json(courses);
 });
 
 module.exports = router;
